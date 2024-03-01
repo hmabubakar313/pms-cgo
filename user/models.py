cd5a49f7f794aaab6cb746591e06fc214a6e9c95
Hamza09s <hamzaaqeel09@gmail.com>
2024-03-01 15:45:33 +0500

    Add property manager and listing

--- user/models.py
+++ user/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
+from django.core.validators import MinValueValidator
 from .managers import CustomUserManager
 
 
@@ -16,3 +16,24 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class PropertyManager(CustomUser):
+    username = models.CharField("username", max_length=100)
+
+    def __str__(self):
+        return self.username
+
+
+class Listing(models.Model):
+    property_manager = models.ForeignKey('PropertyManager', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    num_bedrooms = models.IntegerField(  validators=[MinValueValidator(0)])
+    num_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.CharField(max_length=100)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
