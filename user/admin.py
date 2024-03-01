4459eb0ec595e39319d939517768c486d69d6c19
Hamza09s <hamzaaqeel09@gmail.com>
2024-03-01 18:32:31 +0500

    model correction

--- user/admin.py
+++ user/admin.py
@@ -5,28 +5,8 @@ from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser, PropertyManager
 
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ("email", "is_staff", "is_active",)
-    list_filter = ("email", "is_staff", "is_active",)
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
-    )
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": (
-                "email", "password1", "password2", "is_staff",
-                "is_active", "groups", "user_permissions"
-            )}
-         ),
-    )
-    search_fields = ("email",)
-    ordering = ("email",)
+class PropertyManagerAdmin(UserAdmin):
+    pass  # Customize admin options if needed
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(PropertyManager)
+admin.site.register(PropertyManager, PropertyManagerAdmin)
--- user/models.py
+++ user/models.py
@@ -18,7 +18,8 @@ class CustomUser(AbstractUser):
         return self.email
 
 
-class PropertyManager(CustomUser):
+class PropertyManager(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     username = models.CharField("username", max_length=100)
 
     def __str__(self):
@@ -27,13 +28,33 @@ class PropertyManager(CustomUser):
 
 class Listing(models.Model):
     property_manager = models.ForeignKey('PropertyManager', on_delete=models.CASCADE)
+    # Tenant = models.ForeignKey('Tenant', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     price = models.IntegerField()
     num_bedrooms = models.IntegerField(  validators=[MinValueValidator(0)])
     num_bathrooms = models.IntegerField()
     square_footage = models.IntegerField()
     address = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = models.ForeignKey('Image', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='image/')
+
+# class Brokers(CustomUser):
+#     property_manager = models.ForeignKey('PropertyManager', on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+
+
+# class Tenant(CustomUser):
+#     property_manager = models.ForeignKey('PropertyManager', on_delete=models.CASCADE)
+#     broker = models.ForeignKey('Broker', on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+
+
+# class Leads(models.Model):
+#     date = models.DateField()
+    
