b1f030edd8b660db4aafb2e31adbdad823bd8481
Hamza09s <hamzaaqeel09@gmail.com>
2024-03-01 14:06:57 +0500

    set up dashboard login and signup

--- ui/urls.py
+++ ui/urls.py
@@ -5,7 +5,11 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('dashboard/', views.base, name="signup"),
+    path('dashboard/',views.dashboard,name="dashboard"),
+    path('login/',views.login,name="login"),
+    path('signup/',views.signup,name="signup"),
+    path('table/',views.table,name="table")
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
