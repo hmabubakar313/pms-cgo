bd4f9b08e0cb44b80a33ae75830d479fa78c2881
Hamza09s <hamzaaqeel09@gmail.com>
2024-03-01 14:30:18 +0500

    form changes

--- ui/urls.py
+++ ui/urls.py
@@ -1,9 +1,8 @@
-
-from django.contrib import admin
 from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('dashboard/',views.dashboard,name="dashboard"),
     path('login/',views.login,name="login"),
@@ -11,5 +10,6 @@ urlpatterns = [
     path('table/',views.table,name="table")
     
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
