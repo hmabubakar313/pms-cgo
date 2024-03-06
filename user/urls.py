d54cce9e29c672c6b9379279ef96532a4a291b57
Hamza09s <hamzaaqeel09@gmail.com>
2024-03-06 15:37:59 +0500

    set up detail view for property listing

--- user/urls.py
+++ user/urls.py
@@ -13,10 +13,12 @@ urlpatterns = [
     path('delete_listing/<int:list_id>',views.delete_listing,name="delete_listing"),
     path('update_listing/<int:list_id>',views.update_listing,name="update_listing"),
     path('create_lead/',views.create_lead,name="create_lead"),
+    path("view_lead/<lead_id>/",views.view_lead,name="view_lead"),
     path('list_leads/',views.list_leads,name="list_leads"),
     path('update_lead/<lead_id>/',views.update_lead,name="update_lead"),
     path('delete_lead/<lead_id>/',views.delete_lead,name="delete_lead"),
     path('create_tenant/',views.create_tenant,name="create_tenant"),
+    path('view_tenant/<tenant_id>/',views.view_tenant,name="view_tenant"),
     path('list_tenants/',views.list_tenants,name="list_tenants"),
     path('delete_tenant/<tenant_id>/',views.delete_tenant,name="delete_tenant"),
     path('update_tenant/<tenant_id>/',views.update_tenant,name="update_tenant"),
@@ -25,6 +27,7 @@ urlpatterns = [
     path("delete_broker/<broker_id>/",views.delete_broker,name="delete_broker"),
     path("update_broker/<broker_id>/",views.update_broker,name="update_broker"),
     path('', views.home, name='home'),
+    path('view_broker/<broker_id>/',views.view_broker,name="view_broker"),
     
 ]
 
